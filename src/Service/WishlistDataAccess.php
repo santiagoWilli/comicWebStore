82c36becc98a138323836c2593530dccbce77426
Stefany Martín Socas <stefany@secret-source.eu>
2020-05-27 20:01:07 +0100

    final de jornada 27m Stefany

--- src/Service/WishlistDataAccess.php
+++ src/Service/WishlistDataAccess.php
@@ -6,7 +6,7 @@ namespace App\Service;
 
 class WishlistDataAccess extends DataAccess
 {
-    public function addToWishlist($userId, $comicId) {
+    public function addToWishlist($comicId, $userId) {
         return parent::executeSQL("INSERT INTO wishlist (comic_id, user_id) 
                                         VALUES (:comic_id, :user_id);", [
             "comic_id" => $comicId,
